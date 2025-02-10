156832e61115bdc3531d5f4512ba6bb5e6b92612
confumbit <confumbit@gmail.com>
2025-02-10 23:58:16 +0530

    add title

--- app/page.jsx
+++ app/page.jsx
@@ -10,9 +10,14 @@ export default function Home() {
           alt="cover art"
           className="h-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10"
         />
-        {/* <div className="font-[Unison] text-7xl text-[#1B1A3A] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20">
-          Aashritha Bidurukontham
-        </div> */}
+        <div className="font-[Unison] text-5xl text-center text-[#ffffff] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20">
+          Aashritha <br /> Bidurukontham
+          <br />
+          <br />
+          <span className="font-sans text-lg font-semibold">
+            product designer
+          </span>
+        </div>
       </div>
     </main>
   );
